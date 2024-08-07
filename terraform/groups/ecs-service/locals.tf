59f93f58f2a72284cb7d0a26f2eda8475f792ca7
Michael Acreman <macreman.ch@gmail.com>
2024-08-07 06:45:18 +0100

    update tf healthcheck path

--- terraform/groups/ecs-service/locals.tf
+++ terraform/groups/ecs-service/locals.tf
@@ -7,7 +7,7 @@ locals {
   container_port              = "8080"
   docker_repo                 = "document-store-delta-consumer"
   kms_alias                   = "alias/${var.aws_profile}/environment-services-kms"
-  healthcheck_path            = "/document-store-delta-consumer/healthcheck"
+  healthcheck_path            = "/healthcheck"
   healthcheck_matcher         = "200"
   vpc_name                    = local.stack_secrets["vpc_name"]
   s3_config_bucket            = data.vault_generic_secret.shared_s3.data["config_bucket_name"]
