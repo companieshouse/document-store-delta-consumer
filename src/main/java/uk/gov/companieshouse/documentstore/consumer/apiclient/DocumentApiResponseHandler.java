8996ba9cfe149c22f3d34ed9b40e9110c2865082
johncookch <jcook@companieshouse.gov.uk>
2024-09-04 11:46:53 +0100

    DSND-3000: Retry more error types
    * All API client errors, apart from 400 and 409, are now retryable
    * Updated the parent POM to pull in the security check changes
    * Updated POM to exclude some dependencies with CVEs. Needs a Spring version upgrade

--- src/main/java/uk/gov/companieshouse/documentstore/consumer/apiclient/DocumentApiResponseHandler.java
+++ src/main/java/uk/gov/companieshouse/documentstore/consumer/apiclient/DocumentApiResponseHandler.java
@@ -2,9 +2,9 @@ package uk.gov.companieshouse.documentstore.consumer.apiclient;
 
 import static uk.gov.companieshouse.documentstore.consumer.Application.NAMESPACE;
 
+import java.util.Arrays;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import uk.gov.companieshouse.api.error.ApiError;
 import uk.gov.companieshouse.api.error.ApiErrorResponseException;
 import uk.gov.companieshouse.api.handler.exception.URIValidationException;
 import uk.gov.companieshouse.documentstore.consumer.exception.NonRetryableException;
@@ -12,8 +12,6 @@ import uk.gov.companieshouse.documentstore.consumer.exception.RetryableException
 import uk.gov.companieshouse.documentstore.consumer.logging.DataMapHolder;
 import uk.gov.companieshouse.logging.Logger;
 import uk.gov.companieshouse.logging.LoggerFactory;
-
-import java.util.List;
 
 @Component
 public class DocumentApiResponseHandler {
@@ -25,23 +23,27 @@ public class DocumentApiResponseHandler {
     private static final String UNEXPECTED_RESPONSE_CODE_MESSAGE = "Unexpected HTTP response code %d when creating document";
 
     public void handle(ApiErrorResponseException ex) {
+
         final int statusCode = ex.getStatusCode();
-        if (HttpStatus.valueOf(statusCode).is5xxServerError()) {
-            LOGGER.info(API_ERROR_RESPONSE_MESSAGE.formatted(statusCode), DataMapHolder.getLogMap());
-            throw new RetryableException(API_ERROR_RESPONSE_MESSAGE.formatted(statusCode), ex);
+        String message = API_ERROR_RESPONSE_MESSAGE.formatted(statusCode);
+
+        if (HttpStatus.BAD_REQUEST.value() == ex.getStatusCode() || HttpStatus.CONFLICT.value() == ex.getStatusCode()) {
+            LOGGER.error(message, ex, DataMapHolder.getLogMap());
+            throw new NonRetryableException(message, ex);
         } else {
-            LOGGER.error(API_ERROR_RESPONSE_MESSAGE.formatted(statusCode), DataMapHolder.getLogMap());
-            throw new NonRetryableException(API_ERROR_RESPONSE_MESSAGE.formatted(statusCode), ex);
+            LOGGER.info(String.format("%s. %s", message, Arrays.toString(ex.getStackTrace())),
+                    DataMapHolder.getLogMap());
+            throw new RetryableException(message, ex);
         }
     }
 
     public void handle(URIValidationException ex) {
-        LOGGER.error(URI_VALIDATION_EXCEPTION_MESSAGE, DataMapHolder.getLogMap());
+        LOGGER.error(URI_VALIDATION_EXCEPTION_MESSAGE, ex, DataMapHolder.getLogMap());
         throw new NonRetryableException(URI_VALIDATION_EXCEPTION_MESSAGE, ex);
     }
 
     public void handleNullResponse() {
-        LOGGER.error(NULL_RESPONSE_EXCEPTION_MESSAGE, DataMapHolder.getLogMap());
+        LOGGER.info(NULL_RESPONSE_EXCEPTION_MESSAGE, DataMapHolder.getLogMap());
         throw new RetryableException(NULL_RESPONSE_EXCEPTION_MESSAGE);
     }
 
