1b5d22f368027ee3e20770a939dee5647869b35f
tmoller-ch <tmoller@companieshouse.gov.uk>
2025-08-19 08:32:22 +0100

    tf changes

--- ecs-image-build/docker_start.sh
+++ ecs-image-build/docker_start.sh
@@ -2,4 +2,4 @@
 
 PORT=8080
 
-exec java -jar -Dserver.port="${PORT}" "document-store-delta-consumer.jar"
+exec java -jar -Dserver.port="${PORT}" -XX:MaxRAMPercentage=80 "document-store-delta-consumer.jar"
